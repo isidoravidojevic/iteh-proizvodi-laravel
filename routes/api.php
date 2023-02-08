7a59a442c3d70fc7d8f0c5fbc8d0382d650cebbf
isidoravidojevic <isidora.vid@gmail.com>
2023-02-08 22:42:48 +0100

    Kontroleri i api rute

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProizvodjacKontroler;
+use App\Http\Controllers\ProizvodKontroler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('proizvodjac', [ProizvodjacKontroler::class, 'store']);
+Route::get('proizvodjac', [ProizvodjacKontroler::class, 'index']);
+Route::delete('proizvodjac', [ProizvodjacKontroler::class, 'destroy']);
+Route::get('proizvod', [ProizvodKontroler::class, 'index']);
+Route::get('proizvod/{id}', [ProizvodKontroler::class, 'show']);
+Route::delete('proizvod/{id}', [ProizvodKontroler::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
